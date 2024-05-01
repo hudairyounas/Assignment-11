917ef494ca88b863adf27bb325269dfa7c99e12c
[Hudair Younis] <[hudairshorts@gmail.com]>
2024-05-01 15:31:03 +0500

    Assignment-11

---
+++ script.js
@@ -0,0 +1,46 @@
+//? - Q1:
+//? let a = 2;
+//? let b = ++a * 2;
+//? b = 6
+
+//? - Q2
+//? let x = 5;
+//? let y = x-- + 2;
+//? y = 7
+
+//? - Q3
+//? let x = 3;
+//? let y = ++x + x++ + ++x;
+//? y = 14
+
+//? - Q4 
+//? let m = 2;
+//? let n = ++m * m++ * --m;
+//? n = 27
+
+//? - Q5
+//? let a = 3;
+//? let b = 5;
+//? let result = ++a + b-- - a++ + --b;
+//? result = 8
+
+//? - Q6
+//? let m = 2;
+//? let n = 4;
+//? let p = m++ + ++n - --m + n--;
+//? m = 1, n = 4, p = 10
+
+//? - Q7
+//? let a = 5;
+//? let b = 3;
+//? let c = 2;
+//? let d = 7;
+//? let result = ++a * (b-- + c) / --d;
+//? a = 6, b = 2, c = 2, d = 6
+
+//? - Q8
+//? let m = 2;
+//? let n = 3;
+//? let o = 4;
+//? let result = m++ * (--n + m) / (o-- - n);
+//? m = 3, o = 3, n = 2
